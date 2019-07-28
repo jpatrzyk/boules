b8d74997c576540990d9021a94e6a886a8c35f61
Joanna Patrzyk <jpatrzyk@users.noreply.github.com>
2019-07-28 20:52:37 +0200

    add material palette & configure colors

--- app/src/components/BoardCell.tsx
+++ app/src/components/BoardCell.tsx
@@ -9,14 +9,6 @@ interface Props {
     onClick: (x: number, y: number) => void;
 }
 
-const Colors = [
-    '',
-    'red',
-    'blue',
-    'green',
-    'yellow'
-];
-
 export const BoardCell: React.FC<Props> = ({ x, y, value, onClick }: Props) => {
     function cellClicked() {
         onClick(x, y);
@@ -26,7 +18,7 @@ export const BoardCell: React.FC<Props> = ({ x, y, value, onClick }: Props) => {
         if (!value) {
             return null;
         }
-        return <div className="BoardCell-circle" style={{ backgroundImage: `radial-gradient(farthest-corner at 60% 40%, white, ${Colors[value]})` }} />;
+        return <div className={`BoardCell-circle BoardCell-circle--${value}`} />;
     }
 
     return (
