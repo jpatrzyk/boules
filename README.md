a54f4c3c15dc47e56120b78a1deaef8bdddff646
Joanna Patrzyk <jpatrzyk@users.noreply.github.com>
2019-07-28 15:09:10 +0200

    Add initial react app & electron skeleton

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# boules
+# Boules
+
+## Getting started
+* Prerequisites: `Node.js` and `yarn`
+* `cd app`
+* `yarn install`
+* `yarn build`
+* `cd ../electron`
+* `yarn install`
+* `yarn start`
---
+++ electron/main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require('electron');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win;
+
+function createWindow () {
+    // Create the browser window.
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    // and load the index.html of the app.
+    win.loadFile('../app/build/index.html');
+
+    // Open the DevTools.
+    win.webContents.openDevTools();
+
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null;
+    });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    app.quit();
+});
