8ae56a2b4b903cba69c412a6bf8a35923ff562b9
Joanna Patrzyk <jpatrzyk@users.noreply.github.com>
2019-07-30 13:20:38 +0200

    use Sass color functions for balls colors

--- app/src/utils/constants.ts
+++ app/src/utils/constants.ts
@@ -1,3 +1,3 @@
 export const NEXT_BALLS_COUNT = 3;
-export const MAX_COLORS_COUNT = 11;
+export const MAX_COLORS_COUNT = 12; // see colors array in ColoredCell.scss
 export const DEFAULT_LINE_LENGTH = 5;
