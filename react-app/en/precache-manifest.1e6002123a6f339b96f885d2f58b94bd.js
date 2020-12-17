33b53b39fd36b4c0f0db4484f8a85d3d5e95a190
Boules CI <jpatrzyk@users.noreply.github.com>
2020-12-17 19:03:27 +0000

    GH Pages

---
+++ react-app/en/precache-manifest.1e6002123a6f339b96f885d2f58b94bd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a67fa1b80a31aa63298d09bf8a9333b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ec438a93b6138ad0dcc",
+    "url": "./static/css/main.a3238e47.chunk.css"
+  },
+  {
+    "revision": "55fcd9760c160a55ea35",
+    "url": "./static/js/2.b1db10d9.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "./static/js/2.b1db10d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ec438a93b6138ad0dcc",
+    "url": "./static/js/main.f69c4d82.chunk.js"
+  },
+  {
+    "revision": "0d4454a6d6bf3b53eaf1",
+    "url": "./static/js/runtime-main.3115220d.js"
+  },
+  {
+    "revision": "c8bb14ceb712288f57e40eda9032bf30",
+    "url": "./static/media/ic_build.c8bb14ce.svg"
+  },
+  {
+    "revision": "fc64499041a08234ad5f03e9aaa3ca13",
+    "url": "./static/media/ic_help.fc644990.svg"
+  },
+  {
+    "revision": "5a52516fc5cde5093c651cb3c1792054",
+    "url": "./static/media/ic_leaderboard.5a52516f.svg"
+  },
+  {
+    "revision": "c275e5189ecaabdbfdc5407dc031c28b",
+    "url": "./static/media/ic_menu.c275e518.svg"
+  },
+  {
+    "revision": "dbafa2b2d358a52cc59e39657aa782a5",
+    "url": "./static/media/ic_open.dbafa2b2.svg"
+  },
+  {
+    "revision": "fbf012c983e6cc2d747cf93eddb0ff0d",
+    "url": "./static/media/ic_refresh.fbf012c9.svg"
+  },
+  {
+    "revision": "1731195fa1171e46db6c945db25dd722",
+    "url": "./static/media/ic_save.1731195f.svg"
+  },
+  {
+    "revision": "61955d8424203cc359e8070f890e1be1",
+    "url": "./static/media/ic_settings.61955d84.svg"
+  },
+  {
+    "revision": "42f85826578323bfe42368c63c93b3c0",
+    "url": "./static/media/ic_undo.42f85826.svg"
+  }
+]);
